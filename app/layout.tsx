cb5f2fc54cb2c0f8df235b9f04fa0e6d8cc19eae
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2023-04-26 12:13:19 +0530

    enhance: add Navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
-"use client" // required since we have used new experimental /app directory
-import { ChakraProvider } from "@chakra-ui/react"
+"use client"; // required since we have used new experimental /app directory
+import Navbar from "@/components/Navbar";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <ChakraProvider>{children}</ChakraProvider></body>
+        <ChakraProvider>
+          <Navbar />
+          {children}
+        </ChakraProvider>
+      </body>
     </html>
-  )
+  );
 }
