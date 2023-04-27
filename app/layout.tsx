36520f2704072dc7f86c49847c575863560933f1
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2023-04-27 17:52:11 +0530

    enhance: add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 "use client"; // required since we have used new experimental /app directory
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -13,6 +14,7 @@ export default function RootLayout({
         <ChakraProvider>
           <Navbar />
           {children}
+          <Footer />
         </ChakraProvider>
       </body>
     </html>
